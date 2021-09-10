7074409259f0e4a5f9e0259123060d37731ab33f
Alison Bessa <alison.bessa@quintoandar.com.br>
2021-09-10 15:17:33 -0300

    import assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import styled from 'styled-components';
+
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
     </div>
   );
 }
