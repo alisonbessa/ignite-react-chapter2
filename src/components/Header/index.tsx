30cf0d2177034c3fd9c852872d1a33094b9ed38e
Alison Bessa <alison.bessa@quintoandar.com.br>
2021-09-17 09:20:19 -0300

    create components - summary

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,10 @@ import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
 export const Header = () => (
-    <Container>
-        <Content>
-            <img src={logoImg} alt="dt money"/>
-            <button type="button">Nova Transação</button>
-        </Content>
-    </Container>
+  <Container>
+    <Content>
+      <img src={logoImg} alt="dt money"/>
+      <button type="button">Nova Transação</button>
+    </Content>
+  </Container>
 )
