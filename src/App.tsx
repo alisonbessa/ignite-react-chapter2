ec6a0fbb9022545537db26f02f68f6f616df5ccb
Alison Bessa <alison.bessa@quintoandar.com.br>
2021-09-17 15:23:02 -0300

    create transaction modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
+import { useState } from 'react';
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { GlobalStyle } from "./styles/global"
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  const handleOpenNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(true);
+  } 
+
+  const handleCloseNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
     </>
   );
 }
